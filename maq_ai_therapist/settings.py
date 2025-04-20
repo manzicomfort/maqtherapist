7c2699dbb3f2f7c07e9f7541db10f85d94f195f6
Tukamushaba Evalyn <2024bit017@std.must.ac.ug>
2025-04-20 18:01:29 +0300

    Fix login redirect

--- maq_ai_therapist/settings.py
+++ maq_ai_therapist/settings.py
@@ -62,13 +62,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'maq_ai_therapist.urls'
@@ -90,6 +90,13 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'maq_ai_therapist.wsgi.application'
+
+# settings.py
+
+LOGIN_URL = 'login'   # (use the name given in your urls.py)
+LOGIN_REDIRECT_URL = '/'  # where to go after successful login
+LOGOUT_REDIRECT_URL = '/login/'  # where to go after logout
+
 
 
 # Database
